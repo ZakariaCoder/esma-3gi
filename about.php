c5b5162901e61288cf3ed872bdeeb602aba18607
ZakariaCoder <ghzzounezakaria@gmail.com>
2025-05-02 02:38:58 +0100

    all done on about page final

--- about.php
+++ about.php
@@ -1,11 +1,3 @@
- <!-- Header-->
-<header class="bg-dark py-5" id="main-header">
-    <div class="container h-100 d-flex align-items-end justify-content-center w-100">
-        <div class="text-center text-white w-100">
-            <h1 class="display-4 fw-bolder mx-5">About Us</h1>
-        </div>
-    </div>
-</header>
 
 <!-- Link to CSS files -->
 <link rel="stylesheet" href="<?php echo base_url ?>assets/css/about.css">
